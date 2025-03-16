71dce30f3030e7510e4b4ae475234f60e633e853
bradytwiz <157566766+bradytwiz@users.noreply.github.com>
2025-03-16 10:02:27 -0700

    Update user_schema.sql

--- src/user_schema.sql
+++ src/user_schema.sql
@@ -1,13 +1,1 @@
--- create the database
-CREATE DATABASE IF NOT EXISTS user_db;
 
--- use scheduler database
-USE user_db;
-
--- create tables for database
-CREATE TABLE IF NOT EXISTS user (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-    userName VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
